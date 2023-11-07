dbfd70ad88781b45e741366ac9647bd36b8ab307
eclipse-34 <109349939+eclipse-34@users.noreply.github.com>
2023-11-08 01:36:12 +0900

    크롤링 완료
    해당 코드는 json파일을 crawl.py가 있는 폴더에 생성하고 이 json 파일을 다른 파일에서 인코딩해서 사용할 수 있습니다.
    titles.json 파일은 crawl.py파일을 실행할 때마다 갱신되고 따라서 crawl.py 파일을 수정함에 따라 다른 조건의 딕셔너리를 얻을 수 있지만, 100개 문장, 5개 기간, 6개 기간인 경우 실행시간이 25분 이상 소요됩니다.

--- crawl.py
+++ crawl.py
@@ -1,33 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
 
 # 키워드 리스트
-keyword_list = ["반도체", "디스플레이", "이차전지", "차세대원전", "수소",
-                "5G", "6G", "바이오", "우주", "항공", "양자", "인공지능", "로봇", "사이버보안"]
+keyword_list = ["반도체", "이차전지", "수소", "우주", "인공지능", "로봇"]
 
 # 각 키워드에 대해 월별 뉴스 타이틀 리스트를 저장할 데이터 구조
-titles = {keyword: [[] for _ in range(12)] for keyword in keyword_list}
+titles = {keyword: [[] for _ in range(5)] for keyword in keyword_list}
 
 user_agent = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
     "Referer": "https://www.google.com"
 }
 
+# 상반기
 for keyword_index, keyword in enumerate(keyword_list):
-    for i in range(1, 2):  # 1월부터 11월(현재 1페이지)
-        month = "{:02d}".format(i)
-        for j in range(1, 5):  # 1페이지부터 50페이지까지(현재 3)
-            start = (j - 1) * 10 + 1
-            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={}&sort=0&photo=0&field=0&pd=3&ds=2023.{}.01&de=2023.{}.31&cluster_rank=749&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from2023{}01to2023{}31,a:all&start={}".format(
-                keyword, month, month, month, month, start)
+    for year in range(2021, 2024):  # 2021년부터 2023년까지
+        for page in range(1, 11):  # 1페이지부터 10페이지까지
+            start = (page - 1) * 10 + 1
+            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={}&sort=0&photo=0&field=0&pd=3&ds={}.01.01&de={}.06.30&cluster_rank=749&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from{}0101to{}0630,a:all&start={}".format(
+                keyword, year, year, year, year, start)
             res = requests.get(url, headers=user_agent)
             soup = BeautifulSoup(res.text, "html.parser")
             print(res.status_code)  # HTTP 상태 코드 확인
-            # 모든 뉴스 타이틀 링크를 찾습니다.
+
             for link in soup.find_all('a', class_='news_tit'):
                 title = link.get('title')
-                titles[keyword][i-1].append(title)
-            print(titles[keyword][i-1])
+                index = (year - 2021) * 2
+                titles[keyword][index].append(title)
+            print(year, index, titles[keyword][index])
+
+            time.sleep(3)
+
+# 하반기
+for keyword_index, keyword in enumerate(keyword_list):
+    for year in range(2021, 2023):  # 2021년부터 2022년까지
+        for page in range(1, 11):  # 1페이지부터 30페이지까지
+            start = (page - 1) * 10 + 1
+            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={}&sort=0&photo=0&field=0&pd=3&ds={}.07.01&de={}.12.31&cluster_rank=749&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from{}0701to{}1231,a:all&start={}".format(
+                keyword, year, year, year, year, start)
+            res = requests.get(url, headers=user_agent)
+            soup = BeautifulSoup(res.text, "html.parser")
+            print(res.status_code)  # HTTP 상태 코드 확인
+
+            for link in soup.find_all('a', class_='news_tit'):
+                title = link.get('title')
+                index = (year - 2021) * 2 + 1
+                titles[keyword][index].append(title)
+            print(year, index, titles[keyword][index])
 
             time.sleep(5)
+
+with open('titles.json', 'w') as file:
+    json.dump(titles, file)
+
+# with open('titles.json', 'r') as file:
+#    titles = json.load(file)
