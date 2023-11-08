15746181d08ba2b885922f9e5097d3f46740e051
eclipse-34 <109349939+eclipse-34@users.noreply.github.com>
2023-11-08 21:56:18 +0900

    2023년 하반기(~10월) 크롤링 추가, 기존 코드 수정

--- mywebsite/crawling/crawl.py
+++ mywebsite/crawling/crawl.py
@@ -50,10 +50,17 @@ for keyword_index, keyword in enumerate(keyword_list):
                 titles[keyword][index].append(title)
             print(year, index, titles[keyword][index])
 
-            time.sleep(5)
+            time.sleep(4)
 
 with open('titles.json', 'w') as file:
     json.dump(titles, file)
 
-# with open('titles.json', 'r') as file:
+
+# with open('titles.json', 'r', encoding='utf-8') as file:
 #    titles = json.load(file)
+
+# titles["반도체"][0]는 반도체 2021년 상반기 기사제목 100개가 있는 리스트
+# titles["로봇"][3]는 로봇 2022년 하반기 기사제목 100개가 있는 리스트
+# 리스트는 주제별, 2021년 상반기부터 2023년 상반기까지 5개 기간, 기사제목 100개로 구성
+
+# print(titles["반도체"][0])
---
+++ mywebsite/crawling/crawl_for_2023_2.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+
+# 키워드 리스트
+keyword_list = ["반도체", "이차전지", "수소", "우주", "인공지능", "로봇"]
+
+# 각 키워드에 대해 월별 뉴스 타이틀 리스트를 저장할 데이터 구조
+titles_2023_2 = {keyword: [] for keyword in keyword_list}
+
+user_agent = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+    "Referer": "https://www.google.com"
+}
+
+
+for keyword in keyword_list:
+    for year in range(2023, 2024):  # 2021년부터 2022년까지
+        for page in range(1, 11):  # 1페이지부터 30페이지까지
+            start = (page - 1) * 10 + 1
+            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={}&sort=0&photo=0&field=0&pd=3&ds=2023.07.01&de=2023.10.31&cluster_rank=749&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from230701to231031,a:all&start={}".format(
+                keyword, start)
+            res = requests.get(url, headers=user_agent)
+            soup = BeautifulSoup(res.text, "html.parser")
+            print(res.status_code)  # HTTP 상태 코드 확인
+
+            for link in soup.find_all('a', class_='news_tit'):
+                title = link.get('title')
+                titles_2023_2[keyword].append(title)
+            print(year, titles_2023_2[keyword])
+
+            time.sleep(4)
+
+with open('titles_2023_2.json', 'w') as file:
+    json.dump(titles_2023_2, file)
